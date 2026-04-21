5c19dbdc9b0fb56aeae95639cfa14ae37bbaaafd
AlexS <skipidarnikov@gmail.com>
2026-04-21 20:31:43 +0300

    fix IP-detect

--- src/weather.config.js
+++ src/weather.config.js
@@ -2,5 +2,5 @@ export default {
   OPEN_METEO_BASE_URL: "https://geocoding-api.open-meteo.com/v1",
   OPEN_METEO_WEATHER_URL: "https://api.open-meteo.com/v1",
   IPIFY_BASE_URL: "https://api.ipify.org",
-  IPAPI_BASE_URL: "https://ipwhois.app/json/",
+  IPAPI_BASE_URL: "https://ipwhois.app/json",
 };
