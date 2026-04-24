77c8e1ae6fa8cbbe3062f013ae9607a107e7db36
AlexS <skipidarnikov@gmail.com>
2026-04-24 18:51:15 +0300

    fix jest threshold

--- jest.config.mjs
+++ jest.config.mjs
@@ -49,10 +49,10 @@ const config = {
   // An object that configures minimum threshold enforcement for coverage results
   coverageThreshold: {
     global: {
-      branches: 60,
-      functions: 60,
-      lines: 60,
-      statements: 60,
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
     },
   },
 
